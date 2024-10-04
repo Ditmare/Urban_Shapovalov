a41562cd5cd307972707ad3c811039e3d963a468
Ditmare <144803026+Ditmare@users.noreply.github.com>
2024-10-05 00:44:54 +0300

    Delete module_1_5.py

--- module_1_5.py
+++
@@ -1,7 +0,0 @@
-mutable_var = (1, 2, 3, 'word', 2.3)
-print(mutable_var)
-#mutable_var[3] = 'delete' ошибка, так как кортеж неизменяем, не поддерживает обращение по элементам(кроме списка внутри)
-mutable_list = [1, 2, 3, 'word', 2.3]
-print(mutable_list)
-mutable_list[3] = 'dsf'
-print(mutable_list)
